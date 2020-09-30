0a386753c5fa2e4786275f3aeefc4b255c959f0f
Arseni Mishutushkin <misharseni@gmail.com>
2020-09-30 21:59:00 +0300

    init: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+  if(arguments.length ==0 || array.length ==0){
+    return 0;
+  }
+    min_result = array[1];
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] < min_result) {
+            min_result = array[i];
+        }
+    }
+    return min_result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if(arguments.length ==0 || array.length ==0){
+    return 0;
+  }
+    max_result = array[1];
+    for (let i = 0; i <= array.length; i++) {
+        if (array[i] > max_result) {
+            max_result = array[i];
+        }
+    }
+    return max_result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if(arguments.length ==0 || array.length ==0){
+    return 0;
+  }
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return (sum / array.length).toFixed(2);
+};
